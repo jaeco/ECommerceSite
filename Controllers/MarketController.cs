351c6f85bf6c7593f196525114a78075fa55700f
James Cor <jaeco@users.noreply.github.com>
2015-06-14 08:18:33 -0600

    Major overhaul on project
    A lot has been added, this is at the point right before connecting the
    CRUD pages to the database.

---
+++ Controllers/MarketController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceSite.Models;
+
+namespace ECommerceSite.Controllers
+{
+    public class MarketController : Controller
+    {
+        public ActionResult Market()
+        {
+            var list = new EcommerceEntities1();
+            var items = list.Products.Where(i => i.Id != null);
+
+            return View(items.ToList());
+        }
+
+        public ActionResult Product(int id)
+        {
+            var list = new EcommerceEntities1();
+            var item = list.Products.Where(i => i.Id == id && i.Id != null).First();
+
+            return View(item);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var list = new EcommerceEntities1();
+            var item = list.Products.Where(i => i.Id == id && i.Id != null).First();
+
+            return View(item);
+        }
+    }
+}
---
+++ Models/SingleProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceSite.Models
+{
+    public class SingleProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Features { get; set; }
+    }
+}
