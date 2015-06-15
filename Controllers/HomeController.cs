b3ede73db5477cf98f37147005ed18ce60ac4bd0
James Cor <jaeco@users.noreply.github.com>
2015-06-15 12:51:58 -0600

    Authentication w/o Promote and Demote

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,17 +9,7 @@ namespace ECommerceSite.Controllers
 {
     public class HomeController : Controller
     {
-        ProductRepo repository = new ProductRepo();
-
         public ActionResult Index()
-        {
-            var list = repository.GetAllProducts();
-            var items = list.Take(3);
-
-            return View(items.ToList());
-        }
-
-        public ActionResult Market()
         {
             return View();
         }
